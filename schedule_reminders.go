d31db0cef48569bb17b45fe87ae0b0ae46c188a1
owenburton <owen.burton13@gmail.com>
2021-03-20 12:01:43 -0600

    🛠️ dont' stop scheduler, send texts every min test

--- schedule_reminders.go
+++ schedule_reminders.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/robfig/cron"
 )
 
 func scheduleReminders() {
 	c := cron.New()
-	reminderIntervals := formatWorkTimes()
 
-	for _, hourInterval := range reminderIntervals {
-		jobStr := "45 " + hourInterval + " * * *"
-		c.AddFunc(jobStr, func() { sendSMS() })
-	}
+	jobStr := "0 * * * * *"
+	c.AddFunc(jobStr, func() { sendSMS() })
+	c.AddFunc(jobStr, func() { fmt.Println("sending text") })
+	// reminderIntervals := formatWorkTimes()
+
+	// for _, hourInterval := range reminderIntervals {
+	// 	fmt.Println("Scheduling reminders @ min 45 of each hour from " + hourInterval)
+	// 	jobStr := "45 " + hourInterval + " * * *"
+	// 	c.AddFunc(jobStr, func() { sendSMS() })
+	// }
 
 	c.Start()
+
+	// c.Stop()
 }
