c0e4adb41bd367d2a79a535a1faf757e8eade0b3
owenburton <owen.burton13@gmail.com>
2021-03-28 13:42:33 -0600

    🧙‍ reduced scope of app, removed google cal api query

--- main.go
+++ main.go
@@ -1,10 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"text/template"
+
+	"github.com/joho/godotenv"
+	"github.com/robfig/cron"
+	"github.com/sfreiberg/gotwilio"
 )
+
+func loadEnvVars() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+}
+
+func sendSMS() {
+	loadEnvVars()
+
+	accountSid := os.Getenv("TWILIO_ACCOUNT_SID")
+	authToken := os.Getenv("TWILIO_AUTH_TOKEN")
+	twilio := gotwilio.NewTwilioClient(accountSid, authToken)
+
+	from := os.Getenv("NUMBER_FROM")
+	to := os.Getenv("NUMBER_TO")
+	message := "Time to meditate, son."
+
+	twilio.SendSMS(from, to, message, "", "")
+}
+
+func scheduleReminders() {
+	c := cron.New()
+
+	jobStr := "0 50 9-20 * * *"
+	c.AddFunc(jobStr, func() { sendSMS() })
+	c.AddFunc(jobStr, func() { fmt.Println("sending text") })
+
+	c.Start()
+}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
